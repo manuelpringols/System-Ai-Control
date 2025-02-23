3924d683ae0d74296c2062e829ddde2c3eabb8a2
manuelpringols <manuelpringols@gmail.com>
2025-02-23 05:50:01 +0100

    Primo commit 23.02.2025 setup struttura progetto

---
+++ backend/src/main/java/com/sistemAi/systemAi/SystemAiIntegrationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sistemAi.systemAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAiIntegrationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemAiIntegrationSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sistemAi/systemAi/SystemAiIntegrationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemAi.systemAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemAiIntegrationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
